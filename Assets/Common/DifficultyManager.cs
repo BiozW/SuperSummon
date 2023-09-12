8908b6bd5c0afac4617d4a65fae3371f145bbbb4
WachirawitPhreuk <wachirawit.pruk@mail.kmutt.ac.th>
2023-09-12 23:41:54 +0700

    EDIT: DifficultyManager

--- Assets/Common/DifficultyManager.cs
+++ Assets/Common/DifficultyManager.cs
@@ -6,6 +6,7 @@ namespace SuperGame
     public class DifficultyManager : Singleton<DifficultyManager>
     {
         public int DifficultyLevel => difficultyLevel;
+        //public ItemSpawner itemSpawner;
         [SerializeField] int difficultyLevel;
         [SerializeField] GameObject difficultyUI;
         
@@ -19,6 +20,27 @@ namespace SuperGame
             difficultyLevel = value;
             difficultyUI.SetActive(false);
             GameManager.Instance.StartLevel();
+
+            if(difficultyLevel == 1)
+            {
+                /*GetComponent<itemSpawner>().heartSpawnRate = 2f;
+                GetComponent<itemSpawner>().sheildSpawnRate = 1f;
+                GetComponent<itemSpawner>().poisonSpawnRate = 0f;*/
+            }
+
+            if(difficultyLevel == 2)
+            {
+                /*GetComponent<itemSpawner>().heartSpawnRate = 1f;
+                GetComponent<itemSpawner>().sheildSpawnRate = 1f;
+                GetComponent<itemSpawner>().poisonSpawnRate = 1f;*/
+            }
+
+            if(difficultyLevel == 3)
+            {
+                /*GetComponent<itemSpawner>().heartSpawnRate = 0f;
+                GetComponent<itemSpawner>().sheildSpawnRate = 0f;
+                GetComponent<itemSpawner>().poisonSpawnRate = 3f;*/
+            }
         }
     }
 }
