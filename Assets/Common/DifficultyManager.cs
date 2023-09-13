5f30e5e4deeac19d3ababeef7cb067e1df691413
BiozW <jirayusuk@gmail.com>
2023-09-13 21:11:36 +0700

    EDIT: Spawner position thigher than before

--- Assets/Common/DifficultyManager.cs
+++ Assets/Common/DifficultyManager.cs
@@ -20,9 +20,10 @@ namespace SuperGame
             difficultyLevel = value;
             difficultyUI.SetActive(false);
             GameManager.Instance.StartLevel();
-
+/*
             if(difficultyLevel == 1)
             {
+                HeartSpawner.Instance
                 GetComponent<HeartSpawner>().heartSpawnRate = 2f;
                 GetComponent<ShieldSpawner>().sheildSpawnRate = 1f;
                 GetComponent<PoisonSpawner>().poisonSpawnRate = 0f;
@@ -30,17 +31,18 @@ namespace SuperGame
 
             if(difficultyLevel == 2)
             {
-                /*GetComponent<itemSpawner>().heartSpawnRate = 1f;
+                GetComponent<itemSpawner>().heartSpawnRate = 1f;
                 GetComponent<itemSpawner>().sheildSpawnRate = 1f;
-                GetComponent<itemSpawner>().poisonSpawnRate = 1f;*/
+                GetComponent<itemSpawner>().poisonSpawnRate = 1f;
             }
 
             if(difficultyLevel == 3)
             {
-                /*GetComponent<itemSpawner>().heartSpawnRate = 0f;
+                GetComponent<itemSpawner>().heartSpawnRate = 0f;
                 GetComponent<itemSpawner>().sheildSpawnRate = 0f;
-                GetComponent<itemSpawner>().poisonSpawnRate = 3f;*/
+                GetComponent<itemSpawner>().poisonSpawnRate = 3f;
             }
+            */
         }
     }
 }
