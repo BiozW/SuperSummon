6313c44d8e66199b6bae361d5c391e8ba3de1348
BiozW <jirayusuk@gmail.com>
2023-09-14 21:16:39 +0700

    EDIT: use old player.cs

--- Assets/DodgeIt/Scripts/Player.cs
+++ Assets/DodgeIt/Scripts/Player.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 namespace SuperGame.DodgeIt
 {
+    /*
     public class Player : MonoBehaviour
     {
         [SerializeField] Rigidbody2D rigidbody;
@@ -25,7 +26,36 @@ namespace SuperGame.DodgeIt
             { 
                 moveDirection *= -1f; 
             } 
-            rigidbody.AddForce(moveDirection * moveSpeed);*/
+            rigidbody.AddForce(moveDirection * moveSpeed);
+        }
+
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.name == "GameOver")
+            {
+                GameManager.Instance.Lose();
+            }
+        }
+    }*/
+    public class Player : MonoBehaviour
+    {
+        [SerializeField] Rigidbody2D rigidbody;
+        
+        public Health Health => health;
+        [SerializeField] Health health;
+        
+        [SerializeField] float moveSpeed = 5f;
+        [SerializeField] float jumpForce = 2f;
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (health.CurrentHealth <= 0)
+                return;
+
+            var horizontalInput = InputManager.Instance.HorizontalInput;
+            var moveDirection = horizontalInput * new Vector3(1, 0, 0);
+            rigidbody.AddForce(moveDirection * moveSpeed);
         }
 
         void OnTriggerEnter2D(Collider2D col)
